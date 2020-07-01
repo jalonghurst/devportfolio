d274935edcbafcd29c94d26436fc5539866bd5c7
jalonghurst <jacqui.longhurst@btinternet.com>
2020-07-01 13:35:27 +0100

    image size change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import initSr from './js/sr';
 
 import './style/main.scss';
 
+
 $('a[href^="#"]').on('click', function(event) {
   var target = $(this.getAttribute('href'));
   if (target.length) {
